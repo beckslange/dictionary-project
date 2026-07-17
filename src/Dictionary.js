d33012021e26568adad904be2ca9800401c35fcb
beckslange <beckslange96@gmail.com>
2026-07-17 15:55:31 -0600

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
-    alert(`Searching for ${keyword}`);
     event.preventDefault();
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiURL).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
