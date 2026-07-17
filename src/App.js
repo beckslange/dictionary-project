fa11b8f1d7fcd3c4d95d56327cc8f1857aaedbaf
beckslange <beckslange96@gmail.com>
2026-07-16 19:41:35 -0600

    checked bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <a href="" className="btn btn-primary">
+          Hello
+        </a>
+      </header>
     </div>
   );
 }
